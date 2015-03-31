84ce2b8ddd18390e2ea2eac1605db17876ba2f27
Ian Remmler <ian@remmler.org>
2015-03-31 08:56:59 -0500

    WIP: App progress.

--- app/setgo/setgo.go
+++ app/setgo/setgo.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/binary"
-	"fmt"
 	"log"
 	"math"
 	"math/rand"
@@ -10,7 +9,6 @@ import (
 
 	"github.com/ianremmler/setgo"
 	"golang.org/x/mobile/app"
-	"golang.org/x/mobile/app/debug"
 	"golang.org/x/mobile/event"
 	"golang.org/x/mobile/f32"
 	"golang.org/x/mobile/geom"
@@ -23,14 +21,16 @@ const (
 )
 
 var (
-	set       *setgo.SetGo
-	program   gl.Program
-	position  gl.Attrib
-	color     gl.Uniform
-	shading   gl.Uniform
-	doOutline gl.Uniform
-	mvMat     gl.Uniform
-	touchLoc  geom.Point
+	set   *setgo.SetGo
+	field []setgo.Card
+
+	on []bool
+
+	program  gl.Program
+	position gl.Attrib
+	color    gl.Uniform
+	shading  gl.Uniform
+	mvMat    gl.Uniform
 
 	cardShape = shape{verts: cardVerts}
 	cardColor = []float32{1, 1, 1, 1}
@@ -38,7 +38,7 @@ var (
 
 var colors = [][]float32{
 	{1.0, 0.0, 0.0, 1.0},
-	{0.0, 1.0, 0.0, 1.0},
+	{0.0, 0.75, 0.0, 1.0},
 	{0.0, 0.0, 1.0, 1.0},
 }
 
@@ -74,6 +74,7 @@ func start() {
 		return
 	}
 
+	gl.LineWidth(4)
 	cardShape.buf = gl.CreateBuffer()
 	vertBytes := f32.Bytes(binary.LittleEndian, cardShape.verts...)
 	gl.BindBuffer(gl.ARRAY_BUFFER, cardShape.buf)
@@ -88,10 +89,10 @@ func start() {
 	position = gl.GetAttribLocation(program, "position")
 	color = gl.GetUniformLocation(program, "color")
 	shading = gl.GetUniformLocation(program, "shading")
-	doOutline = gl.GetUniformLocation(program, "doOutline")
 	mvMat = gl.GetUniformLocation(program, "mvMat")
 
-	touchLoc = geom.Point{geom.Width / 2, geom.Height / 2}
+	field = set.Field()
+	on = make([]bool, len(field))
 }
 
 func stop() {
@@ -102,69 +103,113 @@ func stop() {
 	}
 }
 
-func touch(t event.Touch) {
-	touchLoc = t.Loc
+func touch(evt event.Touch) {
+	if evt.Type != event.TouchEnd {
+		return
+	}
+
+	w, h, fw, fh := viewDims()
+	dw, dh := float32(geom.Width), float32(geom.Height)
+	tx, ty := float32(evt.Loc.X), float32(evt.Loc.Y)
+
+	rows, cols := 3, len(field)/3
+	c := int(float32(cols) * (tx/dw*w/fw - 0.5*(w-fw)/fw))
+	r := int(float32(rows) * (ty/dh*h/fh - 0.5*(h-fh)/fh))
+
+	idx := -1
+	if r > 0 && r <= rows && c > 0 && c <= cols {
+		idx = 3*c + r
+	}
+
+	// XXX {{{
+	if idx > 0 {
+		on[idx] = !on[idx]
+	}
+	// XXX }}}
 }
 
-func drawCard(card setgo.Card) {
-	// 	num, clr, shp, fil := card.Attr[0], card.Attr[1], card.Attr[2], card.Attr[3]
+// func drawCard(mat *f32.Mat4, card *setgo.Card) {
+func drawCard(mat *f32.Mat4, idx int) {
+	card := field[idx]
+	num, clr, shp, fil := card.Attr[0], card.Attr[1], card.Attr[2], card.Attr[3]
+
+	mvMat.WriteMat4(mat)
+
 	gl.BindBuffer(gl.ARRAY_BUFFER, cardShape.buf)
 	gl.EnableVertexAttribArray(position)
-	gl.VertexAttribPointer(position, coordsPerVertex, gl.FLOAT, false, 0, 0)
+	gl.VertexAttribPointer(position, 3, gl.FLOAT, false, 0, 0)
 
 	gl.Uniform1i(shading, 2)
 	gl.Uniform4fv(color, cardColor)
-	gl.DrawArrays(gl.TRIANGLE_FAN, 0, len(cardShape.verts))
-	gl.Uniform4f(color, 0, 0, 0, 0)
-	gl.DrawArrays(gl.LINE_LOOP, 0, len(cardShape.verts))
-
+	gl.DrawArrays(gl.TRIANGLE_FAN, 0, len(cardShape.verts)/3)
+	if on[idx] {
+		gl.Uniform4f(color, 1, 0, 0, 1)
+	} else {
+		gl.Uniform4f(color, 0, 0, 0, 1)
+	}
+	gl.DrawArrays(gl.LINE_LOOP, 0, len(cardShape.verts)/3)
 	gl.DisableVertexAttribArray(position)
-	// 	gl.Uniform4fv(color, colors[clr])
+
+	gl.BindBuffer(gl.ARRAY_BUFFER, shapes[shp].buf)
+	gl.EnableVertexAttribArray(position)
+	gl.VertexAttribPointer(position, 3, gl.FLOAT, false, 0, 0)
+	gl.Uniform4fv(color, colors[clr])
+
+	for i := 0; i <= num; i++ {
+		shapeMat := *mat
+		offset := float32(i+1) / (float32(num) + 2)
+		shapeMat.Translate(&shapeMat, 0.5, offset*cardAspRat, 0)
+		shapeMat.Scale(&shapeMat, 0.1, 0.1, 0)
+		mvMat.WriteMat4(&shapeMat)
+
+		gl.Uniform1i(shading, fil)
+		gl.DrawArrays(gl.TRIANGLE_FAN, 0, len(shapes[shp].verts)/3)
+		gl.Uniform1i(shading, 2)
+		gl.DrawArrays(gl.LINE_LOOP, 0, len(shapes[shp].verts)/3)
+	}
+	gl.DisableVertexAttribArray(position)
 }
 
-func draw() {
-	field := set.Field()
+func viewDims() (float32, float32, float32, float32) {
 	cols := len(field) / 3
 	fieldAspRat := float32(cols) / (3 * cardAspRat)
 	dispAspRat := float32(geom.Width / geom.Height)
-	// 	aspRat := dispAspRat / fieldAspRat
 
-	ow, oh := float32(cols), float32(3*cardAspRat)
-	w, h := ow, oh
+	fieldWidth, fieldHeight := float32(cols), float32(3*cardAspRat)
+	width, height := fieldWidth, fieldHeight
 	if dispAspRat > fieldAspRat {
-		w = oh * dispAspRat
+		width = fieldHeight * dispAspRat
 	} else {
-		h = ow / dispAspRat
+		height = fieldWidth / dispAspRat
 	}
-	fmt.Println(" ->", w, h)
-	// 	if aspRat > 1 {
-	// 		w = h * aspRat
-	// 	} else {
-	// 		h = w / aspRat
-	// 	}
+	// margin
+	width *= 1.01
+	height *= 1.01
+	return width, height, fieldWidth, fieldHeight
+}
 
-	gl.ClearColor(0, 0, 0, 1)
-	gl.Clear(gl.COLOR_BUFFER_BIT)
-
-	gl.UseProgram(program)
-
-	// 	gl.Uniform4f(color, 0, green, 0, 1)
-	// 	gl.Uniform2f(offset, float32(touchLoc.X/geom.Width), float32(touchLoc.Y/geom.Height))
-
+func viewMat(w, h, fw, fh float32) f32.Mat4 {
 	mat := f32.Mat4{}
 	mat.Identity()
-	// 	mat.Scale(&mat, 0.5*h, 0.5*w, 1)
 	mat.Scale(&mat, 1.0/(0.5*w), 1.0/(0.5*h), 1)
+	return mat
+}
+
+func draw() {
+	gl.ClearColor(0, 0, 0, 1)
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+	gl.UseProgram(program)
+
+	w, h, fw, fh := viewDims()
+	mat := viewMat(w, h, fw, fh)
 	for i := range field {
 		x, y := float32(i/3), cardAspRat*float32(i%3)
-		mv := mat
-		fmt.Println("i:", i, "x:", x, "y:", y, "w:", w, "h:", h, "px:", x-0.5*w, "py:", y-0.5*h)
-		mv.Translate(&mv, x-0.5*ow, y-0.5*oh, 0)
-		mvMat.WriteMat4(&mv)
-		drawCard(field[i])
+		cardMat := mat
+		cardMat.Translate(&cardMat, x-0.5*fw, y-0.5*fh, 0)
+		// 		drawCard(&cardMat, &field[i])
+		drawCard(&cardMat, i)
 	}
-
-	debug.DrawFPS()
+	// 	debug.DrawFPS()
 }
 
 var cardVerts = []float32{
@@ -174,33 +219,29 @@ var cardVerts = []float32{
 	0, cardAspRat, 0,
 }
 
-var squareVerts = []float32{
-	0, 0, 0,
-	1, 0, 0,
-	1, 1, 0,
-	0, 1, 0,
-}
-
 var sin60 = float32(math.Sqrt(3) / 2)
 
+var squareVerts = []float32{
+	-sin60, -sin60, 0,
+	sin60, -sin60, 0,
+	sin60, sin60, 0,
+	-sin60, sin60, 0,
+}
+
 var triVerts = []float32{
-	-sin60, 0, 0,
-	0.5, 1, 0,
-	sin60, 0, 0,
+	-sin60, -sin60, 0,
+	0, sin60, 0,
+	sin60, -sin60, 0,
 }
 
 var hexVerts = []float32{
-	-sin60, 0, 0,
-	-1, 0.5, 0,
-	-sin60, 1, 0,
-	sin60, 1, 0,
-	1, 0.5, 0,
-	sin60, 0, 0,
+	-0.5, -sin60, 0,
+	-1, 0, 0,
+	-0.5, sin60, 0,
+	0.5, sin60, 0,
+	1, 0, 0,
+	0.5, -sin60, 0,
 }
-
-const (
-	coordsPerVertex = 3
-)
 
 const vertShader = `#version 100
 uniform mat4 mvMat;
